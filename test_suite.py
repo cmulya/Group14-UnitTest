ca5a7f599898106f3073024c9c39d9e3a55bcf2c
Baldeep Dhada <bdhada@student.ubc.ca>
2023-12-08 13:46:03 -0800

    workout_test_suit

--- test_suite.py
+++ test_suite.py
@@ -1,11 +1,32 @@
 import unittest
+from warmup_test import TestWarmup
+from workout_test import TestWorkout
 from planoptions_test import TestPlanOptions
 from dietoptions_test import TestDietOptions
 
 
 def my_suite():
+    # workout plan
     suite = unittest.TestSuite()
     result = unittest.TestResult()
+    suite.addTest(TestWarmup('setUpClass'))
+    suite.addTest(TestWarmup('tearDownClass'))
+    suite.addTest(TestWarmup('setUp'))
+    suite.addTest(TestWarmup('tearDown'))
+    suite.addTest(TestWarmup('test_athlete_warmup'))
+    suite.addTest(TestWarmup('test_strength_warmup'))
+    suite.addTest(TestWarmup('test_bodyweight_warmup'))
+    suite.addTest(TestWarmup('test_youtube_links_warmup'))
+    suite.addTest(TestWorkout('setUpClass'))
+    suite.addTest(TestWorkout('tearDownClass'))
+    suite.addTest(TestWorkout('setUp'))
+    suite.addTest(TestWorkout('tearDown'))
+    suite.addTest(TestWorkout('test_generate_intensity_plan'))
+    suite.addTest(TestWorkout('test_generate_powerlifting_plan'))
+    suite.addTest(TestWorkout('test_generate_calisthenics_plan'))
+    suite.addTest(TestWorkout('test_youtube_links'))
+
+    # Diet plan
     suite.addTest(TestPlanOptions('setUpClass'))
     suite.addTest(TestPlanOptions('tearDownClass'))
     suite.addTest(TestPlanOptions('setUp'))
