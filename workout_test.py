356dfc13f9470e4c73764bfbbdc12e85b3ca2721
Baldeep Dhada <bdhada@student.ubc.ca>
2023-12-08 13:22:01 -0800

    updated testing files

--- workout_test.py
+++ workout_test.py
@@ -30,6 +30,11 @@ class TestWorkout(unittest.TestCase):
         self.calisthenics_workout_sixty = workout.WorkOut(60, "body weight")
         self.calisthenics_workout_sixty_two = workout.WorkOut(62, "body weight")
         self.calisthenics_workout_negative = workout.WorkOut(-5, "body weight")
+        
+        self.youtube_invalid_workout = workout.WorkOut(32, "invalid")
+        self.youtube_athlete = workout.WorkOut(32, "athlete")
+        self.youtube_powerlifting = workout.WorkOut(52, "strength")
+        self.youtube_calisthenics = workout.WorkOut(23, "body weight")
     
     @classmethod
     def setUpClass(cls):
@@ -63,4 +68,21 @@ class TestWorkout(unittest.TestCase):
         self.assertEqual(self.calisthenics_workout_sixty_two.generate_calisthenics_plan(), None)
         self.assertEqual(self.calisthenics_workout_negative.generate_calisthenics_plan(), None)
     
+    def test_youtube_links(self):
+        self.assertEqual(self.youtube_invalid_workout.youtube_links(), None)
+        push_press = "https://www.youtube.com/watch?v=iaBVSJm78ko"
+        squat_jumps = "https://www.youtube.com/watch?v=YGGq0AE5Uyc"
+        pull_ups = "https://www.youtube.com/watch?v=XB_7En-zf_M"
+        self.assertEqual(self.youtube_athlete.youtube_links(), f'{push_press}\n{squat_jumps}\n{pull_ups}')
+        bench_press = "https://www.youtube.com/shorts/EdDqD4aKwxM"
+        barbell_squats = "https://www.youtube.com/shorts/gslEzVggur8"
+        deadlifts = "https://www.youtube.com/shorts/McCDaAsSeRc"
+        self.assertEqual(self.youtube_powerlifting.youtube_links(), f'{bench_press}\n{barbell_squats}\n{deadlifts}')
+        push_ups = "https://www.youtube.com/watch?v=IODxDxX7oi4"
+        pistol_squats = "https://www.youtube.com/shorts/5R3wjhmjhgU"
+        pull_ups = "https://www.youtube.com/watch?v=XB_7En-zf_M"
+        self.assertEqual(self.youtube_calisthenics.youtube_links(), f'{push_ups}\n{pistol_squats}\n{pull_ups}')
+    
+    
+       
 unittest.main(argv =[''], verbosity=2, exit=False)
